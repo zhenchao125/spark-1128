fb4dcdbd052b5a811b7ea3c6de42214755b09995
lzc <30752740@qq.com>
2020-05-06 14:39:07 +0800

    更新

--- spark-core/src/main/scala/com/atguigu/spark/core/day03/Practice2.scala
+++ spark-core/src/main/scala/com/atguigu/spark/core/day03/Practice2.scala
@@ -1,6 +1,5 @@
 package com.atguigu.spark.core.day03
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -13,7 +12,20 @@ object Practice2 {
         // 10分钟
         val conf: SparkConf = new SparkConf().setAppName("Practice2").setMaster("local[2]")
         val sc: SparkContext = new SparkContext(conf)
-        
+        sc
+            .textFile("c:/1128.txt")
+            .flatMap(_.split("\\W+"))
+            .map((_, 1))
+            .aggregateByKey(0)(_ + _, _ + _)
+            .map {
+                case (k, v) => (v, k)
+            }
+            .sortByKey(ascending = false)
+            .map {
+                case (v, k) => (k, v)
+            }
+            .collect
+            .foreach(println)
         
         sc.stop()
         
