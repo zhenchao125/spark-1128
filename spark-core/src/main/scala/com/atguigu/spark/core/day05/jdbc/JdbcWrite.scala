f4ebcf40fd20684ec30fd50013135a4f18503532
lzc <30752740@qq.com>
2020-05-09 11:37:19 +0800

    更新

--- spark-core/src/main/scala/com/atguigu/spark/core/day05/jdbc/JdbcWrite.scala
+++ spark-core/src/main/scala/com/atguigu/spark/core/day05/jdbc/JdbcWrite.scala
@@ -23,11 +23,11 @@ object JdbcWrite {
             .textFile("c:/1128.txt")
             .flatMap(_.split("\\W+"))
             .map((_, 1))
-            .reduceByKey(_ + _)
+            .reduceByKey(_ + _, 3)
         
         // 只能手动去写
         val sql = "insert into word_count1128 values(?, ?)"
-        wordCount.foreach {
+        /*wordCount.foreach {
             case (word, count) =>
                 // 加载驱动
                 Class.forName(driver)
@@ -41,7 +41,50 @@ object JdbcWrite {
                 // 关闭连接
                 ps.close()
                 conn.close()
-        }
+        }*/
+        
+        /*wordCount.foreachPartition( it => {
+            // it就是存储的每个分区数据
+            // 建立到mysql的连接
+            Class.forName(driver)
+            // 获取连接
+            val conn = DriverManager.getConnection(url, user, pw)
+            
+            it.foreach{
+                case (word, count) =>
+                    val ps = conn.prepareStatement(sql)
+                    ps.setString(1, word)
+                    ps.setInt(2, count)
+                    ps.execute()
+                    ps.close()
+            }
+            conn.close()
+        } )*/
+        
+        wordCount.foreachPartition(it => {
+            // it就是存储的每个分区数据
+            // 建立到mysql的连接
+            Class.forName(driver)
+            // 获取连接
+            val conn = DriverManager.getConnection(url, user, pw)
+            val ps = conn.prepareStatement(sql)
+            var max = 0 // 最大批次
+            it.foreach {
+                case (word, count) =>
+                    ps.setString(1, word)
+                    ps.setInt(2, count)
+                    ps.addBatch()
+                    max += 1
+                    if (max >= 10) {
+                        ps.executeBatch()
+                        max = 0
+                    }
+            }
+            // 最后一次不到提交的上限,  做收尾
+            ps.executeBatch()
+            conn.close()
+        })
+        
         
         sc.stop()
         
